cd3d2a858b3bebe3169dfd990dec492423615730
BMZOOO <76647795+BMZOOO@users.noreply.github.com>
2021-01-05 17:06:32 +0300

    Initial Commit

---
+++ classWork6/classWork6App.swift
@@ -0,0 +1,17 @@
+//
+//  classWork6App.swift
+//  classWork6
+//
+//  Created by Batool  Jawaheri  on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct classWork6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
